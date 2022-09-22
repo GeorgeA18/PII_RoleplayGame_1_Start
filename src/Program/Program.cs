5e66690883b2de3d8b93fd4619a5bdf2d2e41ae8
Milagros Varela <mvarelagodoy@gmail.com>
2022-09-21 23:19:03 -0300

    Instancias de items

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 
-namespace Program
+namespace RPG
 {
-    class Program
+    class Game
     {
         static void Main(string[] args)
         {
+            //Instancias para cada personaje
+            //Falta completar el funcionamiento de inventory
+            Wizard harry = new Wizard("Harry");
+            harry.Health = 100;
+
+            Dwarf gernis = new Dwarf("Gernis");
+            gernis.Health = 100;
+
+            Elf gimbel = new Elf("Gimbel");
+            gimbel.Health = 100;
+
+            //Instancias para cada item
+            Gear robe = new Gear("Robe", false);
+            Gear gown = new Gear("Gown", false);
+            Gear wristlet = new Gear("Wristlet", false);
+            
+            Protection armor = new Protection("Armor", 40, false);
+            Protection copperShield = new Protection("Copper Shield", 10, false);
+            Protection silverShield = new Protection("Silver Shield", 20, false);
+            Protection helmet = new Protection("Helmet", 10, false);
+            Protection gauntlet = new Protection("Gauntlet", 10, false);
+
+            Weapon sword = new Weapon("Sword", 10, false);
+            Weapon axe = new Weapon("Axe", 10, false);
+            Weapon spear = new Weapon("Spear", 10, false);
+            Weapon dagger = new Weapon("Dagger", 10, false);
+            Weapon crossbow = new Weapon("Crossbow", 20, false);
+            Weapon magicSword = new Weapon("Magic Sword", 20, true);
+
+            DualItem spikedShield = new DualItem("Spiked Shield", 10, 20, false);
+            DualItem wand = new DualItem("Wand", 20, 20, true);
+            DualItem scepter = new DualItem("Scepter", 20, 20, true);
+
         }
     }
 }
